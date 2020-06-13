53e6e489f8caafac3a193ac48a84ed3796d73f4c
Anurag Saini <anurag.inias@gmail.com>
2020-06-13 18:54:15 +0530

    First commit

---
+++ src/main/java/dev/anurag/Solution.java
@@ -0,0 +1,9 @@
+package dev.anurag;
+
+public class Solution {
+
+    public static final void main(String[] args) {
+        AddressBookProtos.AddressBook book = AddressBookProtos.AddressBook.newBuilder().build();
+        System.out.println("Hello");
+    }
+}
